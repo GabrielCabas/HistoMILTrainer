efedbcaf1970428054b6aeda6f49acd86ed35b97
GabrielCabas <gabriel.cabas@outlook.com>
2026-01-19 13:45:14 -0300

    new class

--- histomil/__init__.py
+++ histomil/__init__.py
@@ -3,4 +3,5 @@ from .datasets import H5Dataset, variable_patches_collate_fn
 from .utils import seed_torch, get_weights, EarlyStopping
 from .train import train, test
 from .models import import_model
-__all__ = ["SplitManager", "H5Dataset", "variable_patches_collate_fn", "seed_torch", "get_weights", "train", "test", "EarlyStopping", "import_model"]
+from .grid_search import GridSearch
+__all__ = ["SplitManager", "H5Dataset", "variable_patches_collate_fn", "seed_torch", "get_weights", "train", "test", "EarlyStopping", "import_model", "GridSearch"]
